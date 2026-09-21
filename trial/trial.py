128d060536695bf763cf69512b9b8ad17662f61b
Jeremy <jtumbur11@gmail.com>
2026-09-21 07:25:09 +0700

    test

---
+++ trial/trial.py
@@ -0,0 +1,15 @@
+identitas={}
+identitas["nama"]="Nobelikiiiii"
+identitas["usia"]=30
+identitas["alamat"]="Tomangacak"
+identitas["pekerjaan"]="AI Automation Engineer"
+identitas["hobi"]=["Coding", "Gaming", "Traveling"]
+identitas["motto hidup"]="Hala Madrid"
+
+print(identitas)
+
+a=Pelajaran
+b=Python
+c=500000000
+
+print(f"{a}+{b}+{c}"
